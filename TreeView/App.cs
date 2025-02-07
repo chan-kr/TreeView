87669db9b695c08acb6356f09b8263889d649005
chan0 <chan0@WIN-KDU3ULVRRA9>
2025-02-07 11:32:47 +0900

    초기 커밋.

---
+++ TreeView/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TreeView
+{   // 어플리케이션을 처음 시작하게 해주는 셋팅, 윈도우 창의 이름을 설정하고 화면에 띄워주는 셋팅.
+    internal class App : Application
+    {   
+        protected override void OnStartup(StartupEventArgs e) // *override 부모 클래스의 메소드를 재정의 할 때 쓰는 키워드
+        {   
+            base.OnStartup(e);                                // base : 부모클래스를 말하는 것 같음.
+            Window window = new();                            // C# 9.0부터는 생성자를 호출할 때 "new()" 만 써도 됌.
+            window.Title = "TreeView";
+            window.ShowDialog();
+
+        }
+    }
+}
---
+++ TreeView/Starter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeView
+{   //프로그램(앱) 시작점. 콘솔 앱으로 따지면 Program 클래스. C언어로 따지면 main 함수 부분.
+    internal class Starter
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            _= new App().Run();                // "_ =" 문법은 이 메서드는 반환값이 있지만, 사용하지 않겠다는 것을 나타내는 문법
+
+                                                // " _= 를 왜써? 그냥 new App().Run()만 쓰면 안돼?" 라고 질문할 수 있음!!!
+                                                //      - Run() 함수가 반환값은 있지만 그 것을 사용하지 않겠다는 것을 명시(가독성)
+                                                //      - new App().Run()만 쓰면 반환값이 있는지 없는지도 구분 어려움.
+                                                //      - 그리고 _= 안쓰면 컴파일러가 왜 반환값 안쓰냐고 자꾸 경고줌. 그거 방지. 
+        }
+    }
+}
+ 
